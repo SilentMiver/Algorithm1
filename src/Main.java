039944263ad377b34b3fbfe52e14372aaf6f7f50
russi <russianthief3@gmail.com>
2023-09-16 16:10:34 +0300

    add generation for minions and listing

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,53 @@
 
 public class Main {
     public static void main(String[] args) {
-        LinkList list = new LinkList();
-        list.addAtFirst("First");
-        list.addAtFirst("Second");
-        list.add("Cocos");
-        list.add(12);
-        list.add('c');
+//        LinkList list = new LinkList();
+//        list.addAtFirst("First");
+//        list.addAtFirst("Second");
+//        list.add("Cocos");
+//        list.add(12);
+//        list.add('c');
+//
+//        list.showAllData();
+//        list.insertAt(3, "Вместо кокоса");
+//        list.showAllData();
+//        list.deleteAt(4);
+//        list.showAllData();
+//        list.add(new Minion(21,"Samuel"));
+//        list.showAllData();
 
-        list.showAllData();
-        list.insertAt(3, "Вместо кокоса");
-        list.showAllData();
-        list.deleteAt(4);
-        list.showAllData();
-        list.add(new Minion(21,"Samuel"));
-        list.showAllData();
         LinkList<Minion> minionLinkList = new LinkList<Minion>();
-        minionLinkList.add(new Minion(21,"Samuel"));
+        minionLinkList.add(new Minion(21, "Samuel"));
         minionLinkList.showAllData();
-        minionLinkList.getValueAt(1).setData(new Minion(1,"Goro"));
+        System.out.println("\n" + "Add Samuel");
 
+        minionLinkList.getValueAt(1).setData(new Minion(21, "Goro"));
         minionLinkList.showAllData();
+        System.out.println("\n" + "Samuel's name now Goro");
+
+        for (int i = 0; i < 3; i++) {
+            minionLinkList.add(new Minion());
+        }
+        minionLinkList.showAllData();
+        System.out.println("\n" + "Add random minions");
+
+        minionLinkList.addAtFirst(new Minion(66, "Jora"));
+        minionLinkList.showAllData();
+        System.out.println("\n" + "Add minion Jora on top");
+
+        minionLinkList.deleteAt(5);
+        minionLinkList.deleteAt(4);
+        minionLinkList.showAllData();
+        System.out.println("\n" + "Delete minions 4-5");
+
+        minionLinkList.insertAt(3, new Minion(12, "Gosha"));
+        minionLinkList.showAllData();
+        System.out.println("\n" + "Add Gosha at index 3");
+
+        minionLinkList.add(new Minion());
+        minionLinkList.showAllData();
+        System.out.println("\n" + "Add random minion");
+
+
     }
 }
--- src/Minion.java
+++ src/Minion.java
@@ -1,4 +1,9 @@
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Objects;
+import java.util.Random;
+
+import static java.time.chrono.JapaneseEra.values;
 
 public class Minion {
     int age;
@@ -8,7 +13,13 @@ public class Minion {
         this.age = age;
         this.name = name;
     }
+    public Minion() {
+        Random random = new Random();
 
+        this.age = random.nextInt(1, 20);
+
+        this.name = Collections.unmodifiableList(Arrays.asList(values())).get(random.nextInt(Collections.unmodifiableList(Arrays.asList(values())).size())).toString();
+    }
 
     public int getAge() {
         return age;
