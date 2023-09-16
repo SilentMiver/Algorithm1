298e83cce283222d39aee3029d4afec96a40b6f5
russi <russianthief3@gmail.com>
2023-09-16 13:28:36 +0300

    add base LinkList realisation with numbers

--- src/LinkList.java
+++ src/LinkList.java
@@ -1,2 +1,31 @@
 public class LinkList {
+    protected Node start; //сс
+    protected Node end;
+    public int size;
+    public LinkList() {
+        start = null;
+        end = null;
+        size = 0;
+    }
+    public void insertAtStart(int val) {
+        Node nptr = new Node(val, null, null);
+        if (start == null) {
+            start = nptr;
+            end = start;
+        } else {
+            start.setLinkPrev(nptr);
+            nptr.setLinkNext(start);
+            start = nptr;
+        }
+        size++;
+    }
+
+    @Override
+    public String toString() {
+        return "LinkList{" +
+                "start=" + start +
+                ", end=" + end +
+                ", size=" + size +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+
 public class Main {
     public static void main(String[] args) {
-
+        LinkList list = new LinkList();
+        list.insertAtStart(1);
+        list.insertAtStart(12);
+        System.out.println(list.toString());
     }
 }
